561e42141761aa6eee8f17fc9627963737db9220
christylydia <christinegikandi@gmail.com>
2019-08-14 16:03:53 +0300

    initial commit

---
+++ index.js/script.js
@@ -0,0 +1,7 @@
+var add = function(firstNumber , secondNumber){
+    return firstNumber + secondNumber ;
+    };
+    var firstNumber= parseInt(prompt("Enter a number:"));
+    var secondNumber=parseInt(prompt ("Enter another number:"));
+    
+    alert(add(firstNumber , secondNumber));
